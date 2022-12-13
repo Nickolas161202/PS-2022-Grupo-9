0bdb59d73999b428b85a8e407dda82b0e0d3afe3
Nickolinhas-rei-delas <cabocanick@gmail.com>
2022-12-13 20:41:48 -0300

    criando componentes

---
+++ react/my-app/src/components/list.js
@@ -0,0 +1,21 @@
+function List () {
+
+<div className="App">
+      <header className="App-header">
+       
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ut delectus necessitatibus aperiam autem, saepe praesentium tenetur dignissimos molestias enim consequuntur distinctio. Excepturi et quae repellendus harum fugiat velit aliquam?
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+}
+
+export default List
