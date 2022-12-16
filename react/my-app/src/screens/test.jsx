77b3efad8e5ccf1aeae28eb5d89f2b01f989a329
Fl4viaMelkor <flaviadelelis@hotmail.com>
2022-12-16 11:28:05 -0300

    Update test.jsx

--- react/my-app/src/screens/test.jsx
+++ react/my-app/src/screens/test.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
 import Atacantes from "./Atacantes"
 import Goleiros from "./Goleiros"
+import MeioCampo from './MeioCampo'
+import Defensores from './Defensores'
+
 
 const Rendering = () =>{
 const [state, setState] = useState()
@@ -10,10 +13,14 @@ return(
 <div>
 
 
-        <button onClick={ () => setState("a")}>Atacantes</button>
-        <button onClick ={() =>{setState("g")}}>goleiros</button>
+        <button onClick={() => setState("a")}>Atacantes</button>
+        <button onClick ={() =>{setState("g")}}>Goleiros</button>
+        <button onClick={()=>{setState("m")}}>Meio Campo</button>
+        <button onClick={()=>{setState("d")}}>Defensores</button>
         { (state == "a")? <div><Atacantes/> </div>:''  }
         { (state == "g")? <div><Goleiros/> </div>:''  }
+        { (state == "m")? <div><MeioCampo/> </div>:''  }
+        { (state == "d")? <div><Defensores/> </div>:''  }
         </div>
 
     )
