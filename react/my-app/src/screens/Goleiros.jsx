b5ca2814197c4c2e0c0a625a64eedef9b31df626
Bruno Costa <brunocosta0427@gmail.com>
2022-12-16 20:26:57 -0300

    fazendo algumas atualizações em relação a responsividade

--- react/my-app/src/screens/Goleiros.jsx
+++ react/my-app/src/screens/Goleiros.jsx
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
+import React from "react"
 import PlayerBlue from "../components/common/PlayerBlue"
 import PlayerGreen from "../components/common/PlayerGreen"
 
-=======
-import React from "react"
-import PlayerGreen from "../components/common/PlayerGreen"
-import PlayerBlue from "../components/common/PlayerBlue"
->>>>>>> 17595d8fb678dba45106d2e262f6d8bdd7aaf045
+
+
+
 const Goleiros = () =>{
  return(
     <div>
