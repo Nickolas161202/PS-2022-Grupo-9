53caada833e6ba22d7ebc6fa2301a8622a0ee77f
lucapcf <119978877+lucapcf@users.noreply.github.com>
2022-12-17 14:30:39 -0300

    sobre

--- react/my-app/src/screens/Sobre.jsx
+++ react/my-app/src/screens/Sobre.jsx
@@ -8,7 +8,7 @@ const Sobre = () => {
             <h1 className={styles.title}>Seleção Brasileira De Futebol</h1>
             
             <div className={styles.header}>
-                <h1 className={styles.textHeader} >SOBRE A SELEÇÃO</h1>
+                <h3>SOBRE A SELEÇÃO</h3>
             </div>
 
             <div className={styles.textContainer}>
