195f073d66d7913d747bc85c597d0e22d57380a6
Bruno Costa <brunocosta0427@gmail.com>
2022-12-17 11:44:57 -0300

    arrumandooo

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
-import MainScreen from "./screens/MainScreen";
-=======
+
 
 import MainScreen from "./screens/Mainscreen";
->>>>>>> 70135a52bf691ab4bee04b83e2f5b067d93b11cf
+
 
 
 function App() {
